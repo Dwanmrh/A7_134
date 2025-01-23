c9b43d2ef7a2da8c1dedfeb9a94e5a2e77765d8e
Ridwan Hidayatullah <ridwanhh10@gmail.com>
2025-01-23 11:01:05 +0700

    Membuat bagian body tampilan form untuk menambah pasien baru dengan button simpan

--- app/src/main/java/com/dwan/ta_pam/ui/view/InsertPasienView.kt
+++ app/src/main/java/com/dwan/ta_pam/ui/view/InsertPasienView.kt
@@ -1,20 +1,28 @@
 package com.dwan.ta_pam.ui.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.dwan.ta_pam.ui.customwidget.CustomTopAppBar
 import com.dwan.ta_pam.ui.navigation.DestinasiNavigasi
 import com.dwan.ta_pam.ui.viewmodel.InsertPasienViewModel
+import com.dwan.ta_pam.ui.viewmodel.InsertUiEvent
+import com.dwan.ta_pam.ui.viewmodel.InsertUiState
 import com.dwan.ta_pam.ui.viewmodel.PenyediaViewModel
 import kotlinx.coroutines.launch
 
@@ -63,5 +71,31 @@ fun EntryPasScreen(
                 .verticalScroll(rememberScrollState())
                 .fillMaxWidth()
         )
+    }
+}
+
+@Composable
+fun EntryBodyPas(
+    insertUiState: InsertUiState,
+    onPasienValueChange: (InsertUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+        FormInputPas(
+            insertUiEvent = insertUiState.insertUiEvent,
+            onValueChange = onPasienValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Simpan")
+        }
     }
 }
