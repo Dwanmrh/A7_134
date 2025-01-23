d9acffdf9d7fd9b2b21da8ff23827986ef8f5499
Ridwan Hidayatullah <ridwanhh10@gmail.com>
2025-01-23 20:56:23 +0700

    Menambahkan gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,4 +81,6 @@ dependencies {
 
     // Untuk kotlin 1.9.10
     implementation("androidx.compose.compiler:compiler:1.5.2")
+
+    implementation("androidx.navigation:navigation-compose:2.7.7") // Sesuaikan versi terbaru
 }
