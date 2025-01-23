e1d5ed8ecd6038fa03ac716e6acc1778bafe1bf5
Ridwan Hidayatullah <ridwanhh10@gmail.com>
2025-01-23 20:58:24 +0700

    Memperbaiki rute pengelola halaman update pasien

--- app/src/main/java/com/dwan/ta_pam/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/dwan/ta_pam/ui/navigation/PengelolaHalaman.kt
@@ -3,51 +3,60 @@ package com.dwan.ta_pam.ui.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.dwan.ta_pam.ui.view.DestinasiEntryPasien
 import com.dwan.ta_pam.ui.view.DestinasiHome
+import com.dwan.ta_pam.ui.view.DestinasiUpdatePasien
 import com.dwan.ta_pam.ui.view.EntryPasScreen
 import com.dwan.ta_pam.ui.view.HomeScreen
+import com.dwan.ta_pam.ui.view.UpdatePasScreen
 
 @Composable
 fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {
-
-    // Mengatur navigasi antar layar menggunakan NavHost
     NavHost(
-        navController = navController, // Controller navigasi untuk mengelola navigasi antar layar
-        startDestination = DestinasiHome.route, // Layar awal adalah HomeScreen
+        navController = navController,
+        startDestination = DestinasiHome.route,
         modifier = Modifier,
     ) {
-        // Halaman Home
         composable(DestinasiHome.route) {
             HomeScreen(
                 navigateToPasienEntry = {
-                    navController.navigate(DestinasiEntryPasien.route) // Navigasi ke EntryPasScreen
+                    navController.navigate(DestinasiEntryPasien.route)
                 },
                 navigateToPasien = {
-                    navController.navigate(DestinasiHome.route) // Navigasi ke Halaman Pasien
+                    navController.navigate(DestinasiHome.route)
                 },
-                navigateToTerapis = {
-                },
-                navigateToJenisTerapi = {
-                },
-                navigateToSesiTerapi = {
-                },
-                onDetailClick = {
+                navigateToTerapis = {},
+                navigateToJenisTerapi = {},
+                navigateToSesiTerapi = {},
+                onDetailClick = {},
+                navigateToUpdatePasien = { id_pasien ->
+                    // Navigasi ke halaman update dengan menyertakan id_pasien sebagai argumen
+                    navController.navigate("${DestinasiUpdatePasien.route}/$id_pasien")
                 }
             )
         }
 
-        // Halaman Entry Pasien
         composable(DestinasiEntryPasien.route) {
             EntryPasScreen(
                 navigateBack = {
-                    navController.navigate(DestinasiHome.route) { // Navigasi kembali ke HomeScreen
-                        popUpTo(DestinasiHome.route) { inclusive = true } // Bersihkan layar sebelumnya dari back stack
+                    navController.navigate(DestinasiHome.route) {
+                        popUpTo(DestinasiHome.route) { inclusive = true }
                     }
                 }
+            )
+        }
+
+        composable(
+            route = DestinasiUpdatePasien.routeWithArgs,
+            arguments = listOf(navArgument(DestinasiUpdatePasien.id_pasien) { type = NavType.StringType })
+        ) { backStackEntry ->
+            UpdatePasScreen(
+                navigateBack = { navController.popBackStack() }
             )
         }
     }
