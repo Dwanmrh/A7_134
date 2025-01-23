35deb7a4a9f2a6ecce396db9837163b4d5c054f3
Ridwan Hidayatullah <ridwanhh10@gmail.com>
2025-01-23 11:42:13 +0700

    Membuat fungsi untuk update data pasien

--- app/src/main/java/com/dwan/ta_pam/ui/viewmodel/UpdatePasienViewModel.kt
+++ app/src/main/java/com/dwan/ta_pam/ui/viewmodel/UpdatePasienViewModel.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.dwan.ta_pam.model.Pasien
 import com.dwan.ta_pam.repository.PasienRepo
 import kotlinx.coroutines.launch
 
@@ -32,6 +33,31 @@ class UpdatePasienViewModel(private val pas: PasienRepo) : ViewModel() {
                 )
             } catch (e: Exception) {
                 // Handle error jika diperlukan
+            }
+        }
+    }
+
+    // Fungsi untuk mengubah data form ketika ada input dari pengguna
+    fun updatePasState(updateUiEvent: UpdateUiEvent) {
+        updateUiState = UpdateUiState(updateUiEvent = updateUiEvent) // Perbarui data berdasarkan event
+    }
+
+    // Fungsi untuk memuat data pasien ke dalam form untuk di-update
+    fun loadPasienData(pasien: Pasien) {
+        updateUiState = pasien.toUpdateUiStatePas()
+    }
+
+    // Fungsi untuk memperbarui data pasien ke database
+    fun updatePas() {
+        viewModelScope.launch { // Menjalankan proses di latar belakang (tidak mengganggu UI)
+            try {
+                // Mengambil data dari form dan mengirimnya ke repository
+                pas.updatePasien(
+                    id_pasien = updateUiState.updateUiEvent.id_pasien, // Ambil ID Pasien dari updateUiState
+                    pasien = updateUiState.updateUiEvent.toPas() // Konversi event menjadi Pasien
+                )
+            } catch (e: Exception) {
+                e.printStackTrace() // Menangani error jika terjadi masalah
             }
         }
     }
