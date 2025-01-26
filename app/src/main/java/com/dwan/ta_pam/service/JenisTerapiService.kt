f65be11cf97fd803ae428a38acc5f39d52acc536
Ridwan Hidayatullah <ridwanhh10@gmail.com>
2025-01-26 12:36:40 +0700

    Membuat service untuk jenis terapi

--- app/src/main/java/com/dwan/ta_pam/service/JenisTerapiService.kt
+++ app/src/main/java/com/dwan/ta_pam/service/JenisTerapiService.kt
@@ -1,4 +1,32 @@
 package com.dwan.ta_pam.service
 
+import com.dwan.ta_pam.model.JenisTerapi
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
+
 interface JenisTerapiService {
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json",
+    )
+    @GET("bacajenis.php")
+    suspend fun getJenis(): List<JenisTerapi>
+
+    @GET("baca1jenis.php/{id_jenis_terapi}")
+    suspend fun getJenisById(@Query("id_jenis_terapi")id_jenis_terapi:String): JenisTerapi
+
+    @POST("insertjenis.php")
+    suspend fun insertJenis(@Body jenisTerapi: JenisTerapi)
+
+    @PUT("editjenis.php/{id_jenis_terapi}")
+    suspend fun updateJenis(@Query("id_jenis_terapi")id_jenis_terapi: String, @Body jenisTerapi: JenisTerapi)
+
+    @DELETE("deletejenis.php/{id_jenis_terapi}")
+    suspend fun deleteJenis(@Query("id_jenis_terapi")id_jenis_terapi: String): Response<Void>
 }
