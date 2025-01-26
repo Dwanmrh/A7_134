2f96707d1e928e5a051ea6b89d652cc4eae3b5ca
Ridwan Hidayatullah <ridwanhh10@gmail.com>
2025-01-26 12:36:52 +0700

    Membuat repository untuk jenis terapi

--- app/src/main/java/com/dwan/ta_pam/repository/JenisTerapiRepo.kt
+++ app/src/main/java/com/dwan/ta_pam/repository/JenisTerapiRepo.kt
@@ -1,2 +1,44 @@
 package com.dwan.ta_pam.repository
 
+import com.dwan.ta_pam.model.JenisTerapi
+import com.dwan.ta_pam.service.JenisTerapiService
+import java.io.IOException
+
+interface JenisTerapiRepo {
+    suspend fun getJenis(): List<JenisTerapi>
+    suspend fun insertJenis(jenisTerapi: JenisTerapi)
+    suspend fun updateJenis(id_jenis_terapi: String, jenisTerapi: JenisTerapi)
+    suspend fun deleteJenis(id_jenis_terapi: String)
+    suspend fun getJenisById(id_jenis_terapi: String): JenisTerapi
+}
+
+class NetworkJenisTerapiRepo(
+    private val jenisTerapiApiService: JenisTerapiService
+) : JenisTerapiRepo {
+    override suspend fun insertJenis(jenisTerapi: JenisTerapi) {
+        jenisTerapiApiService.insertJenis(jenisTerapi)
+    }
+
+    override suspend fun updateJenis(id_jenis_terapi: String, jenisTerapi: JenisTerapi) {
+        jenisTerapiApiService.updateJenis(id_jenis_terapi, jenisTerapi)
+    }
+
+    override suspend fun deleteJenis(id_jenis_terapi: String) {
+        try {
+            val response = jenisTerapiApiService.deleteJenis(id_jenis_terapi)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete Jenis Terapi. HTTP Status code: ${response.code()}")
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    override suspend fun getJenis(): List<JenisTerapi> = jenisTerapiApiService.getJenis()
+    override suspend fun getJenisById(id_jenis_terapi: String): JenisTerapi {
+        return jenisTerapiApiService.getJenisById(id_jenis_terapi)
+    }
+}
