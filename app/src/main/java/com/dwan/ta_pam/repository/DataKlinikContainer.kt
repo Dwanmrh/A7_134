eb106f44dd1bda66403441fb1f705c9a8d6ae84a
Ridwan Hidayatullah <ridwanhh10@gmail.com>
2025-01-26 12:41:18 +0700

    Mendeklarasikan repository terapis, jenis terapi, dan sesi terapi di container

--- app/src/main/java/com/dwan/ta_pam/repository/DataKlinikContainer.kt
+++ app/src/main/java/com/dwan/ta_pam/repository/DataKlinikContainer.kt
@@ -1,6 +1,9 @@
 package com.dwan.ta_pam.repository
 
+import com.dwan.ta_pam.service.JenisTerapiService
 import com.dwan.ta_pam.service.PasienService
+import com.dwan.ta_pam.service.SesiService
+import com.dwan.ta_pam.service.TerapisService
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
@@ -8,6 +11,9 @@ import retrofit2.Retrofit
 
 interface AppContainer {
     val pasienRepo: PasienRepo
+    val terapisRepo: TerapisRepo
+    val jenisTerapiRepo: JenisTerapiRepo
+    val sesiTerapiRepo: SesiTerapiRepo
 
 }
 
@@ -21,9 +27,27 @@ class DataKlinikContainer : AppContainer {
     private val pasienService: PasienService by lazy {
         retrofit.create(PasienService::class.java)
     }
+    private val terapisService: TerapisService by lazy {
+        retrofit.create(TerapisService::class.java)
+    }
+    private val jenisService: JenisTerapiService by lazy {
+        retrofit.create(JenisTerapiService::class.java)
+    }
+    private val sesiService: SesiService by lazy {
+        retrofit.create(SesiService::class.java)
+    }
 
 
     override val pasienRepo: PasienRepo by lazy {
         NetworkPasienRepo(pasienService)
+    }
+    override val terapisRepo: TerapisRepo by lazy {
+        NetworkTerapisRepo(terapisService)
+    }
+    override val jenisTerapiRepo: JenisTerapiRepo by lazy {
+        NetworkJenisTerapiRepo(jenisService)
+    }
+    override val sesiTerapiRepo: SesiTerapiRepo by lazy {
+        NetworkSesiTerapiRepo(sesiService)
     }
 }
