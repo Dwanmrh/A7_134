40b9fcfae25baa184f028dd7cb3c7553638efd6e
Ridwan Hidayatullah <ridwanhh10@gmail.com>
2025-01-28 15:58:35 +0700

    Menambahkan foto pasien

--- app/src/main/java/com/dwan/ta_pam/ui/view/HomePasienView.kt
+++ app/src/main/java/com/dwan/ta_pam/ui/view/HomePasienView.kt
@@ -3,6 +3,7 @@ package com.dwan.ta_pam.ui.view
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,10 +17,13 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
@@ -65,21 +69,21 @@ object DestinasiHome : DestinasiNavigasi {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
-    navigateToPasienEntry: () -> Unit, // Navigasi ke layar tambah data
-    navigateToPasien: () -> Unit, // Navigasi ke halaman Pasien
-    navigateToTerapis: () -> Unit, // Navigasi ke halaman Terapis
-    navigateToJenisTerapi: () -> Unit, // Navigasi ke halaman Jenis Terapi
-    navigateToSesiTerapi: () -> Unit, // Navigasi ke halaman Sesi Terapi
+    navigateToPasienEntry: () -> Unit,
+    navigateToPasien: () -> Unit,
+    navigateToTerapis: () -> Unit,
+    navigateToJenisTerapi: () -> Unit,
+    navigateToSesiTerapi: () -> Unit,
     modifier: Modifier = Modifier,
-    onDetailClick: (String) -> Unit = {}, // Navigasi ke halaman detail
+    onDetailClick: (String) -> Unit = {},
     navigateToUpdatePasien: (String) -> Unit,
-    viewModel: HomePasienViewModel = viewModel(factory = PenyediaViewModel.Factory) // ViewModel untuk mengelola data
+    viewModel: HomePasienViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
     Scaffold(
         modifier = modifier
             .nestedScroll(scrollBehavior.nestedScrollConnection)
-            .background(MaterialTheme.colorScheme.background),
+            .background(Color(0xFF121212)), // Warna latar belakang gelap
         topBar = {
             CustomTopAppBar(
                 title = DestinasiHome.titleRes,
@@ -93,10 +97,10 @@ fun HomeScreen(
         floatingActionButton = {
             FloatingActionButton(
                 onClick = navigateToPasienEntry,
-                shape = MaterialTheme.shapes.medium,
+                shape = CircleShape,
                 modifier = Modifier.padding(18.dp),
-                contentColor = Color.White,
-                containerColor = MaterialTheme.colorScheme.primary
+                containerColor = Color(0xFF8B0000),
+                contentColor = Color.White // Warna teks putih
             ) {
                 Icon(imageVector = Icons.Default.Add, contentDescription = "Add Pasien")
             }
@@ -107,9 +111,6 @@ fun HomeScreen(
                 onTerapisClick = navigateToTerapis,
                 onJenisTerapiClick = navigateToJenisTerapi,
                 onSesiTerapiClick = navigateToSesiTerapi,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp)
             )
         }
     ) { innerPadding ->
@@ -117,7 +118,9 @@ fun HomeScreen(
             homeUiState = viewModel.pasUiState,
             retryAction = { viewModel.getPas() },
             modifier = Modifier.padding(innerPadding),
-            onDetailClick = onDetailClick,
+            onDetailClick = { id_pasien ->
+                onDetailClick(id_pasien)
+            },
             onDeleteClick = { pasien ->
                 viewModel.deletePas(pasien.id_pasien)
                 viewModel.getPas()
@@ -137,26 +140,176 @@ fun HomeStatus(
     onDetailClick: (String) -> Unit
 ) {
     when (homeUiState) {
-        is HomeUiState.Loading -> OnLoading(modifier = modifier.fillMaxSize()) // Menampilkan loading
+        is HomeUiState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
         is HomeUiState.Success -> {
             if (homeUiState.pasien.isEmpty()) {
-                // Tampilkan pesan jika data kosong
                 Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                    Text(text = "Tidak ada data Mahasiswa")
+                    Text(text = "Tidak ada data Pasien", style = MaterialTheme.typography.titleMedium)
                 }
             } else {
-                // Tampilkan daftar mahasiswa
                 PasLayout(
                     pasien = homeUiState.pasien,
                     modifier = modifier.fillMaxWidth(),
                     navigateToUpdatePasien = navigateToUpdatePasien,
-                    onDetailClick = { onDetailClick(it.id_pasien) }, // Mengarahkan ke detail
+                    onDetailClick = { onDetailClick(it.id_pasien) },
                     onDeleteClick = { onDeleteClick(it) }
                 )
             }
         }
-        is HomeUiState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize()) // Tampilkan pesan error
+        is HomeUiState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
     }
+}
+
+@Composable
+fun PasLayout(
+    pasien: List<Pasien>,
+    modifier: Modifier = Modifier,
+    navigateToUpdatePasien: (String) -> Unit,
+    onDetailClick: (Pasien) -> Unit,
+    onDeleteClick: (Pasien) -> Unit = {}
+) {
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(pasien) { pasien ->
+            PasCard(
+                pasien = pasien,
+                modifier = Modifier
+                    .fillMaxWidth(),
+                navigateToUpdatePasien = navigateToUpdatePasien,
+                onDeleteClick = { onDeleteClick(pasien) },
+                onDetailClick = onDetailClick
+            )
+        }
+    }
+}
+
+@Composable
+fun PasCard(
+    pasien: Pasien,
+    modifier: Modifier = Modifier,
+    navigateToUpdatePasien: (String) -> Unit,
+    onDeleteClick: (Pasien) -> Unit = {},
+    onDetailClick: (Pasien) -> Unit
+) {
+    var deleteConfirmationRequired by remember { mutableStateOf(false) }
+
+    Card(
+        modifier = modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .shadow(8.dp, shape = RoundedCornerShape(16.dp))
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = rememberRipple(color = Color.White) // Efek ripple putih
+            ) { onDetailClick(pasien)  },
+        shape = RoundedCornerShape(16.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color(0xFF8B0000), // Warna merah gelap
+            contentColor = Color.White // Warna teks putih
+        )
+    ) {
+        Row(
+            modifier = Modifier.padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            // Gambar profil
+            Image(
+                painter = painterResource(id = R.drawable.pasien),
+                contentDescription = "Foto Pasien",
+                modifier = Modifier
+                    .size(48.dp)
+                    .clip(MaterialTheme.shapes.small) // Bentuk lingkaran
+            )
+            Spacer(Modifier.width(16.dp)) // Jarak antar elemen
+
+            // Informasi Pasien
+            Column(
+                modifier = Modifier.weight(1f),
+                verticalArrangement = Arrangement.spacedBy(4.dp)
+            ) {
+                Text(
+                    text = pasien.nama_pasien,
+                    style = MaterialTheme.typography.titleLarge,
+                    color = Color.White // Warna teks putih
+                )
+                Text(
+                    text = pasien.id_pasien,
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = Color.White.copy(alpha = 0.8f) // Warna teks putih dengan transparansi
+                )
+                Text(
+                    text = pasien.alamat,
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = Color.White.copy(alpha = 0.8f) // Warna teks putih dengan transparansi
+                )
+            }
+
+            // Tombol Edit
+            IconButton(
+                onClick = { navigateToUpdatePasien(pasien.id_pasien) },
+                modifier = Modifier.size(32.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit Pasien",
+                    tint = Color.White // Warna ikon putih
+                )
+            }
+
+            // Tombol Hapus
+            IconButton(
+                onClick = { deleteConfirmationRequired = true },
+                modifier = Modifier.size(32.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = null,
+                    tint = Color.White // Warna ikon putih
+                )
+            }
+        }
+    }
+
+    // Dialog Konfirmasi Hapus
+    if (deleteConfirmationRequired) {
+        DeleteConfirmationDialog(
+            onDeleteConfirm = {
+                deleteConfirmationRequired = false
+                onDeleteClick(pasien)
+            },
+            onDeleteCancel = { deleteConfirmationRequired = false }
+        )
+    }
+}
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    AlertDialog(
+        onDismissRequest = { onDeleteCancel() },
+        title = { Text("Delete Data", color = Color.White) },
+        text = { Text("Apakah anda yakin ingin menghapus data ini?", color = Color.White) },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(text = "Cancel", color = Color.White)
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = "Yes", color = Color.White)
+            }
+        },
+        containerColor = Color(0xFF8B0000), // Warna merah gelap
+        textContentColor = Color.White, // Warna teks putih
+        titleContentColor = Color.White // Warna teks putih
+    )
 }
 
 @Composable
@@ -190,137 +343,4 @@ fun OnError(retryAction: () -> Unit, modifier: Modifier = Modifier) {
             Text(stringResource(R.string.retry))
         }
     }
-}
-
-@Composable
-fun PasLayout(
-    pasien: List<Pasien>,
-    modifier: Modifier = Modifier,
-    navigateToUpdatePasien: (String) -> Unit,
-    onDetailClick: (Pasien) -> Unit,
-    onDeleteClick: (Pasien) -> Unit = {}
-) {
-    LazyColumn(
-        modifier = modifier,
-        contentPadding = PaddingValues(16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
-        items(pasien) { pasien ->
-            PasCard(
-                pasien = pasien,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .clickable { onDetailClick(pasien) }, // Fungsi klik untuk detail
-                navigateToUpdatePasien = navigateToUpdatePasien,
-                onDeleteClick = { onDeleteClick(pasien) }
-            )
-        }
-    }
-}
-
-@Composable
-fun PasCard(
-    pasien: Pasien,
-    modifier: Modifier = Modifier,
-    navigateToUpdatePasien: (String) -> Unit,
-    onDeleteClick: (Pasien) -> Unit = {}
-) {
-    var deleteConfirmationRequired by remember { mutableStateOf(false) }
-
-    Card(
-        modifier = modifier
-            .padding(8.dp)
-            .fillMaxWidth()
-            .shadow(8.dp, shape = MaterialTheme.shapes.medium),
-        shape = MaterialTheme.shapes.medium,
-        colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.primary
-        )
-    ) {
-        Row(
-            modifier = Modifier.padding(16.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            // Informasi Pasien
-            Column(
-                modifier = Modifier.weight(1f),
-                verticalArrangement = Arrangement.spacedBy(4.dp)
-            ) {
-                Text(
-                    text = pasien.nama_pasien,
-                    style = MaterialTheme.typography.titleLarge,
-                    color = MaterialTheme.colorScheme.secondaryContainer
-                )
-                Text(
-                    text = pasien.id_pasien,
-                    style = MaterialTheme.typography.bodyMedium,
-                    color = MaterialTheme.colorScheme.secondaryContainer
-                )
-                Text(
-                    text = pasien.alamat,
-                    style = MaterialTheme.typography.bodyMedium,
-                    color = MaterialTheme.colorScheme.secondaryContainer
-                )
-            }
-
-            // Tombol Edit
-            IconButton(
-                onClick = { navigateToUpdatePasien(pasien.id_pasien) }, // Navigasi ke halaman update
-                modifier = Modifier.size(32.dp)
-            ) {
-                Icon(
-                    imageVector = Icons.Default.Edit,
-                    contentDescription = "Edit Pasien",
-                    tint = MaterialTheme.colorScheme.error
-                )
-            }
-
-            // Tombol Hapus
-            IconButton(
-                onClick = { deleteConfirmationRequired = true },
-                modifier = Modifier.size(32.dp)
-            ) {
-                Icon(
-                    imageVector = Icons.Default.Delete,
-                    contentDescription = null,
-                    tint = MaterialTheme.colorScheme.error
-                )
-            }
-        }
-    }
-
-    // Dialog Konfirmasi Hapus
-    if (deleteConfirmationRequired) {
-        DeleteConfirmationDialog(
-            onDeleteConfirm = {
-                deleteConfirmationRequired = false
-                onDeleteClick(pasien)
-            },
-            onDeleteCancel = { deleteConfirmationRequired = false }
-        )
-    }
-}
-
-@Composable
-private fun DeleteConfirmationDialog(
-    onDeleteConfirm: () -> Unit,
-    onDeleteCancel: () -> Unit,
-    modifier: Modifier = Modifier
-) {
-    AlertDialog(
-        onDismissRequest = { onDeleteCancel() },
-        title = { Text("Delete Data") },
-        text = { Text("Apakah anda yakin ingin menghapus data ini?") },
-        modifier = modifier,
-        dismissButton = {
-            TextButton(onClick = onDeleteCancel) {
-                Text(text = "Cancel")
-            }
-        },
-        confirmButton = {
-            TextButton(onClick = onDeleteConfirm) {
-                Text(text = "Yes")
-            }
-        }
-    )
 }
