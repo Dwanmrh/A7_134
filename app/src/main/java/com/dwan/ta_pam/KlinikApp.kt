aa3218695e64a216dcbb21a1ea09ef5c760c365f
Ridwan Hidayatullah <ridwanhh10@gmail.com>
2025-01-28 16:21:53 +0700

    Merapihkan klinik app

--- app/src/main/java/com/dwan/ta_pam/KlinikApp.kt
+++ app/src/main/java/com/dwan/ta_pam/KlinikApp.kt
@@ -1,22 +1,13 @@
 package com.dwan.ta_pam
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import com.dwan.ta_pam.ui.navigation.PengelolaHalaman
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun KlinikApp(modifier: Modifier = Modifier) {
-
-
-            // Menampilkan halaman dengan navigasi
-            PengelolaHalaman()
-        }
-
+    // Menampilkan halaman dengan navigasi
+        PengelolaHalaman()
+}
