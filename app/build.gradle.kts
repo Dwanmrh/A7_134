bc62b6b6d55eb8378dca63a3670560b9ff6faa31
Ridwan Hidayatullah <ridwanhh10@gmail.com>
2025-01-28 15:36:04 +0700

    Menambahkan beberapa penyesuaian agar project stabil

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,4 +83,8 @@ dependencies {
     implementation("androidx.compose.compiler:compiler:1.5.2")
 
     implementation("androidx.navigation:navigation-compose:2.7.7") // Sesuaikan versi terbaru
+
+    // Icon Terbaru
+    implementation("androidx.compose.material:material-icons-extended:1.6.0")
+
 }
