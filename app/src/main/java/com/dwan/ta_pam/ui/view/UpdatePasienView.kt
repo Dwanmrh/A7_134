8209555e8f442f254993230882c6338986e75d72
Ridwan Hidayatullah <ridwanhh10@gmail.com>
2025-01-28 16:01:17 +0700

    Mengubah warna click button

--- app/src/main/java/com/dwan/ta_pam/ui/view/UpdatePasienView.kt
+++ app/src/main/java/com/dwan/ta_pam/ui/view/UpdatePasienView.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -17,14 +18,13 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.compose.rememberNavController
 import com.dwan.ta_pam.ui.customwidget.CustomTopAppBar
 import com.dwan.ta_pam.ui.navigation.DestinasiNavigasi
 import com.dwan.ta_pam.ui.viewmodel.PenyediaViewModel
@@ -103,7 +103,11 @@ fun UpdateBodyPas(
         Button(
             onClick = onSaveClick,
             shape = MaterialTheme.shapes.small,
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF8B0000), // Warna merah gelap
+                contentColor = Color.White // Warna teks putih
+            )
         ) {
             Text(text = "Update")
         }
