51be85d214274dcf55d33ff0c55f3581d76bee17
Ridwan Hidayatullah <ridwanhh10@gmail.com>
2025-01-28 16:12:46 +0700

    Menyesuaikan penamaan comment

--- app/src/main/java/com/dwan/ta_pam/ui/viewmodel/UpdateJenisTerapiViewModel.kt
+++ app/src/main/java/com/dwan/ta_pam/ui/viewmodel/UpdateJenisTerapiViewModel.kt
@@ -6,9 +6,7 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.dwan.ta_pam.model.JenisTerapi
-import com.dwan.ta_pam.model.Terapis
 import com.dwan.ta_pam.repository.JenisTerapiRepo
-import com.dwan.ta_pam.repository.TerapisRepo
 import kotlinx.coroutines.launch
 
 class UpdateJenisTerapiViewModel(private val jns: JenisTerapiRepo) : ViewModel() {
@@ -17,7 +15,7 @@ class UpdateJenisTerapiViewModel(private val jns: JenisTerapiRepo) : ViewModel()
     var updateJUiState by mutableStateOf(UpdateJUiState())
         private set
 
-    // Fungsi untuk mendapatkan data mahasiswa berdasarkan NIM
+    // Fungsi untuk mendapatkan data jenis terapi berdasarkan ID
     fun getJenisTerapiById(id_jenis_terapi: String) {
         viewModelScope.launch {
             try {
@@ -40,19 +38,19 @@ class UpdateJenisTerapiViewModel(private val jns: JenisTerapiRepo) : ViewModel()
         updateJUiState = UpdateJUiState(updateJUiEvent = updateJUiEvent) // Perbarui data berdasarkan event
     }
 
-    // Fungsi untuk memuat data mahasiswa ke dalam form untuk di-update
+    // Fungsi untuk memuat data jenis terapi ke dalam form untuk di-update
     fun loadJenisTerapiData(jenisTerapi: JenisTerapi) {
         updateJUiState = jenisTerapi.toUpdateUiStateJenisTerapi()
     }
 
-    // Fungsi untuk memperbarui data mahasiswa ke database
+    // Fungsi untuk memperbarui data jenis terapi ke database
     fun updateJenisTerapi() {
         viewModelScope.launch { // Menjalankan proses di latar belakang (tidak mengganggu UI)
             try {
                 // Mengambil data dari form dan mengirimnya ke repository
                 jns.updateJenis(
-                    id_jenis_terapi = updateJUiState.updateJUiEvent.id_jenis_terapi, // Ambil NIM dari updateUiState
-                    jenisTerapi = updateJUiState.updateJUiEvent.toJns() // Konversi event menjadi Mahasiswa
+                    id_jenis_terapi = updateJUiState.updateJUiEvent.id_jenis_terapi, // Ambil ID dari updateUiState
+                    jenisTerapi = updateJUiState.updateJUiEvent.toJns() // Konversi event menjadi jenis terapi
                 )
             } catch (e: Exception) {
                 e.printStackTrace() // Menangani error jika terjadi masalah
@@ -66,28 +64,28 @@ data class UpdateJUiState(
     val updateJUiEvent: UpdateJUiEvent = UpdateJUiEvent() // State default berisi objek kosong dari UpdateUiEvent
 )
 
-// Menyimpan data input pengguna untuk form update mahasiswa
+// Menyimpan data input pengguna untuk form update jenis terapi
 data class UpdateJUiEvent(
     val id_jenis_terapi: String = "",
     val nama_jenis_terapi: String = "",
     val deskripsi_terapi: String = ""
 )
 
-// Fungsi untuk mengubah data UpdateUiEvent menjadi Mahasiswa
-fun UpdateJUiEvent.toJns(): JenisTerapi = JenisTerapi( // UpdateUiEvent > Mahasiswa > Simpan data Pas ke db
-    id_jenis_terapi = id_jenis_terapi, // Memindahkan nilai NIM dari UpdateUiEvent ke Mahasiswa
+// Fungsi untuk mengubah data UpdateUiEvent menjadi jenis terapi
+fun UpdateJUiEvent.toJns(): JenisTerapi = JenisTerapi( // UpdateUiEvent > jenis terapi > Simpan data Pas ke db
+    id_jenis_terapi = id_jenis_terapi, // Memindahkan nilai ID dari UpdateUiEvent ke jenis terapi
     nama_jenis_terapi = nama_jenis_terapi,
     deskripsi_terapi = deskripsi_terapi
 )
 
-// Fungsi untuk mengubah data Mahasiswa menjadi UpdateUiState
-fun JenisTerapi.toUpdateUiStateJenisTerapi(): UpdateJUiState = UpdateJUiState( // Mahasiswa > updateUiEvent > Masuk ke UpdateUiState
-    updateJUiEvent = toUpdateJUiEvent() // Memanggil fungsi toUpdateUiEvent untuk mengonversi data Mahasiswa
+// Fungsi untuk mengubah data jenis terapi menjadi UpdateUiState
+fun JenisTerapi.toUpdateUiStateJenisTerapi(): UpdateJUiState = UpdateJUiState( // jenis terapi > updateUiEvent > Masuk ke UpdateUiState
+    updateJUiEvent = toUpdateJUiEvent() // Memanggil fungsi toUpdateUiEvent untuk mengonversi data jenis terapi
 )
 
-// Fungsi untuk mengubah data Mahasiswa menjadi data UpdateUiEvent
+// Fungsi untuk mengubah data jenis terapi menjadi data UpdateUiEvent
 fun JenisTerapi.toUpdateJUiEvent(): UpdateJUiEvent = UpdateJUiEvent(
-    id_jenis_terapi = id_jenis_terapi, // Memindahkan nilai NIM dari Mahasiswa ke UpdateUiEvent
+    id_jenis_terapi = id_jenis_terapi, // Memindahkan nilai ID dari jenis terapi ke UpdateUiEvent
     nama_jenis_terapi = nama_jenis_terapi,
     deskripsi_terapi = deskripsi_terapi
 )
