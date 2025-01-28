f90c180cf69bdb0c05c0bb199d5b1db41ea43e6f
Ridwan Hidayatullah <ridwanhh10@gmail.com>
2025-01-28 16:08:32 +0700

    Mengubah warna click button

--- app/src/main/java/com/dwan/ta_pam/ui/view/UpdateTerapisView.kt
+++ app/src/main/java/com/dwan/ta_pam/ui/view/UpdateTerapisView.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -19,6 +20,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
@@ -101,7 +103,11 @@ fun UpdateBodyTerapis(
         Button(
             onClick = onSaveClick,
             shape = MaterialTheme.shapes.small,
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF8B0000), // Warna merah gelap
+                contentColor = Color.White // Warna teks putih
+            )
         ) {
             Text(text = "Update")
         }
